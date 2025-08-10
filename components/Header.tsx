4d0150718670cbfbf158a302f914d5c024c06027
dsarfati75 <dsarfati@gmail.com>
2025-08-10 07:43:36 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,9 +17,9 @@ export default function Header() {
 
         <nav className="ml-auto hidden md:flex items-center gap-6 text-sm">
           <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/services">Services</Link>
-          <Link href="/contact">Contact</Link>
+          <Link href="/#about">About</Link>
+          <Link href="/#services">Services</Link>
+          <Link href="/#contact">Contact</Link>
         </nav>
       </div>
     </header>
